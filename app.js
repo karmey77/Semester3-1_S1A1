b952162afdf9aa69edb75bf1fd3a343b24fcdf30
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-07-17 00:07:39 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -26,15 +26,15 @@ app.use(session({
 }))
 
 usePassport(app) // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
-// app.use(flash())
-// app.use((req, res, next) => {
-//     // 你可以在這裡 console.log(req.user) 等資訊來觀察
-//     res.locals.isAuthenticated = req.isAuthenticated()
-//     res.locals.user = req.user
-//     res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
-//     res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
-//     next()
-// })
+app.use(flash())
+app.use((req, res, next) => {
+    // 你可以在這裡 console.log(req.user) 等資訊來觀察
+    res.locals.isAuthenticated = req.isAuthenticated()
+    res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+    res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+    next()
+})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
