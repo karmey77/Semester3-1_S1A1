49466eb2d9ae7c67e55bb92a631fa9860078444b
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-07-17 01:20:33 +0800

    feat: add facebook login & dotenv

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,10 @@ const users = require('./modules/users')
 const restaurants = require('./modules/restaurants') // 引入 restaurants 模組程式碼
 const sort = require('./modules/sort')
 const search = require('./modules/search') // 引入 search 模組程式碼
+const auth = require('./modules/auth')
 
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/search', authenticator, search)
 router.use('/sort', authenticator, sort)
 router.use('/restaurants', authenticator, restaurants) // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組
