4af7089940ee0c00ea8de489f525250b4bac7342
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-07-17 00:58:27 +0800

    feat: add relations between User and Todo models

--- models/restaurant.js
+++ models/restaurant.js
@@ -37,6 +37,12 @@ const restaurantSchema = new Schema({
     description: {
         type: String, // 資料型別是字串
         required: true // 這是個必填欄位
+    },
+    userId: {  // 加入關聯設定
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        index: true,
+        required: true
     }
 })
 
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -9,7 +9,10 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+    const userId = req.user._id
     const restaurant = req.body
+    restaurant["userId"] = userId
+
     return Restaurant.create(restaurant)     // 存入資料庫
         .then(() => res.redirect('/')) // 新增完成後導回首頁
         .catch(error => console.log(error))
@@ -17,40 +20,57 @@ router.post('/', (req, res) => {
 
 // detail
 router.get('/:restaurant_id', (req, res) => {
-    const id = req.params.restaurant_id
-    return Restaurant.find()
+    const userId = req.user._id
+    const _id = req.params.restaurant_id
+
+    return Restaurant.findOne({ _id, userId })
         .lean()
-        .then((restaurants) => restaurants.filter(restaurant => restaurant._id.toString() === id))
-        .then((restaurant) => restaurant[0])
-        .then((restaurant) => res.render('show', { restaurant: restaurant }))
+        .then(restaurant => res.render('show', { restaurant }))
         .catch(error => console.log(error))
+
+    // return Restaurant.find()
+    //     .lean()
+    //     .then((restaurants) => restaurants.filter(restaurant => restaurant._id.toString() === id))
+    //     .then((restaurant) => restaurant[0])
+    //     .then((restaurant) => res.render('show', { restaurant: restaurant }))
+    //     .catch(error => console.log(error))
 })
 
 // edit
 router.get('/:restaurant_id/edit', (req, res) => {
-    const id = req.params.restaurant_id
-    return Restaurant.find()
+    const _id = req.params.restaurant_id
+    const userId = req.user._id
+
+    return Restaurant.findOne({ _id, userId })
         .lean()
-        .then((restaurants) => restaurants.filter(restaurant => restaurant._id.toString() === id))
-        .then((restaurant) => restaurant[0])
-        .then((restaurant) => res.render('edit', { restaurant: restaurant }))
+        .then(restaurant => res.render('edit', { restaurant }))
         .catch(error => console.log(error))
+
+    // return Restaurant.find()
+    //     .lean()
+    //     .then((restaurants) => restaurants.filter(restaurant => restaurant._id.toString() === id))
+    //     .then((restaurant) => restaurant[0])
+    //     .then((restaurant) => res.render('edit', { restaurant: restaurant }))
+    //     .catch(error => console.log(error))
 })
 
 router.put('/:restaurant_id', (req, res) => {
     console.log(req.params.restaurant_id)
     console.log(req.body)
-    const id = req.params.restaurant_id
-    const name = req.body.name
-    const name_en = req.body.name_en
-    const category = req.body.category
-    const location = req.body.location
-    const google_map = req.body.google_map
-    const phone = req.body.phone
-    const description = req.body.description
-    const image = req.body.image
+    const userId = req.user._id
+    const _id = req.params.restaurant_id
 
-    return Restaurant.findById(id)
+    const { name, name_en, category, location, google_map, phone, description, image } = req.body
+    // const name = req.body.name
+    // const name_en = req.body.name_en
+    // const category = req.body.category
+    // const location = req.body.location
+    // const google_map = req.body.google_map
+    // const phone = req.body.phone
+    // const description = req.body.description
+    // const image = req.body.image
+
+    return Restaurant.findOne({ _id, userId })
         .then(restaurant => {
             restaurant.name = name
             restaurant.name_en = name_en
@@ -63,19 +83,41 @@ router.put('/:restaurant_id', (req, res) => {
 
             return restaurant.save()
         })
-        .then(() => res.redirect(`/restaurants/${id}`))
+        .then(() => res.redirect(`/restaurants/${_id}`))
         .catch(error => console.log(error))
+
+    // return Restaurant.findById(id)
+    //     .then(restaurant => {
+    //         restaurant.name = name
+    //         restaurant.name_en = name_en
+    //         restaurant.category = category
+    //         restaurant.location = location
+    //         restaurant.google_map = google_map
+    //         restaurant.phone = phone
+    //         restaurant.description = description
+    //         restaurant.image = image
+
+    //         return restaurant.save()
+    //     })
+    //     .then(() => res.redirect(`/restaurants/${id}`))
+    //     .catch(error => console.log(error))
 })
 
 // delete
 router.delete('/:id', (req, res) => {
-    const id = req.params.id
-    return Restaurant.findById(id)
-        .then(restaurant => {
-            return restaurant.deleteOne()
-        })
+    const _id = req.params.id
+    const userId = req.user._id
+
+    return Restaurant.findOne({ _id, userId })
+        .then(restaurant => restaurant.remove())
         .then(() => res.redirect('/'))
         .catch(error => console.log(error))
+    // return Restaurant.findById(id)
+    //     .then(restaurant => {
+    //         return restaurant.deleteOne()
+    //     })
+    //     .then(() => res.redirect('/'))
+    //     .catch(error => console.log(error))
 })
 
 module.exports = router
