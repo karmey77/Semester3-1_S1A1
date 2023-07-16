ca3a23f1a1f0a2a2bf822c83b9e828eb24c55310
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-07-16 23:06:51 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,18 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
-
-const users = require('./modules/users')
-router.use('/users', users)
-
-const search = require('./modules/search') // 引入 search 模組程式碼
-router.use('/search', search)
-
-const sort = require('./modules/sort')
-router.use('/sort', sort)
-
-const restaurants = require('./modules/restaurants') // 引入 restaurants 模組程式碼
-router.use('/restaurants', restaurants) // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組
-
+const { authenticator } = require('../middleware/auth')  // 掛載 middleware
 const home = require('./modules/home') // 引入 home 模組程式碼
-router.use('/', home) // 將網址結構符合 / 字串的 request 導向 home 模組
+const users = require('./modules/users')
+const restaurants = require('./modules/restaurants') // 引入 restaurants 模組程式碼
+const sort = require('./modules/sort')
+const search = require('./modules/search') // 引入 search 模組程式碼
+
+router.use('/users', users)
+router.use('/search', authenticator, search)
+router.use('/sort', authenticator, sort)
+router.use('/restaurants', authenticator, restaurants) // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組
+router.use('/', authenticator, home) // 將網址結構符合 / 字串的 request 導向 home 模組
 
 // 匯出路由器
 module.exports = router
